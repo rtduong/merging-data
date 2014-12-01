14d5a732dbe48465bc6b9e140dcbff337b0e141a
rtduong@uw.edu <rtduong@uw.edu>
2014-11-30 23:11:20 -0800

    Added functionality
    index.html and app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+var AddressBook = angular.module('AddressBook', []);
+AddressBook.controller('AddressController', ['$scope', function($scope) {
+    $scope.employees = pawneeEmployees;
+    $scope.order = 'lastName';
+}]);
